2ff7120748f80240d9f56d242512b6771dcabce5
Edgar <sayedgarend@gmail.com>
2021-03-31 12:24:45 -0300

    legends

--- js/main.js
+++ js/main.js
@@ -68,6 +68,21 @@ getJSON('/json/cidades.json', function (err, res) {
             .addTo(overLayers[layer + ' (Choropleth)']);
     });
 });
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 13 , 14],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + numberToColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(map);
 
 var criadorPopup = function (layer, cidade) {
     var numALunos = get(layer + cidade),
